4de8aaedc2a0e09f9b80745002e8f751347b077d
ganzander <ganeshmangla2003@gmail.com>
2024-11-02 13:10:57 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { HoveredLink, Menu, MenuItem } from "@/components/ui/navbar-menu";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/components/ui/navbar-menu";
 import {
   IconArrowLeft,
   IconBrandTabler,
   IconSettings,
   IconGardenCart,
   IconTruckDelivery,
-  IconEditCircle,
   IconSun,
 } from "@tabler/icons-react";
 import { cn } from "@/lib/utils";
@@ -56,12 +60,34 @@ function Navbar({ className, theme, changeTheme }) {
         </div>
 
         <div className="w-[70%] md:w-[50%] flex justify-around items-center">
-          <Link href="/lens">
-            <MenuItem setActive={setActive} active={active} item="Lens" />
-          </Link>
-          <Link href="/frames">
-            <MenuItem setActive={setActive} active={active} item="Frames" />
-          </Link>
+          <MenuItem setActive={setActive} active={active} item="Products">
+            <div className="text-sm grid lg:grid-cols-2 gap-10 p-4">
+              <ProductItem
+                title="Power Glasses"
+                href="/lens"
+                src="https://images.unsplash.com/photo-1456081101716-74e616ab23d8?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                description="Experience clear vision with our high-quality power glasses designed for everyday comfort and style."
+              />
+              <ProductItem
+                title="Sunglasses"
+                href="/sunglasses"
+                src="https://images.unsplash.com/photo-1501619838605-f3e4c602db04?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                description="Protect your eyes from UV rays while staying fashionable with our range of trendy sunglasses."
+              />
+              <ProductItem
+                title="Contact Lens"
+                href="/contact-lens"
+                src="https://images.unsplash.com/photo-1659351423172-bb8d42079d6d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29udGFjdCUyMGxlbnN8ZW58MHx8MHx8fDA%3D"
+                description="Discover the convenience and clarity of our high-quality contact lenses for all-day comfort."
+              />
+              <ProductItem
+                title="Accessories"
+                href="/accessories"
+                src="https://images.unsplash.com/photo-1628771065117-74ccb5690668?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGV5ZSUyMG1lZGljaW5lc3xlbnwwfHwwfHx8MA%3D%3D"
+                description="Complete your look with our range of stylish and functional eyewear accessories."
+              />
+            </div>
+          </MenuItem>
 
           {decoded ? (
             <MenuItem
@@ -82,11 +108,6 @@ function Navbar({ className, theme, changeTheme }) {
                 <HoveredLink href="/user/settings">
                   <MenuItemContent label="Settings" Icon={IconSettings} />
                 </HoveredLink>
-                {decoded.isAdmin && (
-                  <HoveredLink href="/user/admin">
-                    <MenuItemContent label="Update" Icon={IconEditCircle} />
-                  </HoveredLink>
-                )}
                 <HoveredLink href="#">
                   <MenuItemContent
                     label="Logout"
--- src/components/ui/apple-cards-carousel.jsx
+++ src/components/ui/apple-cards-carousel.jsx
@@ -15,6 +15,7 @@ import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useOutsideClick } from "@/hooks/use-outside-click";
+import { useRouter } from "next/navigation";
 
 export const CarouselContext = createContext({
   onCardClose: () => {},
@@ -56,7 +57,7 @@ export const Carousel = ({ items, initialScroll = 0 }) => {
 
   const handleCardClose = (index) => {
     if (carouselRef.current) {
-      const cardWidth = isMobile() ? 230 : 384; // (md:w-96)
+      const cardWidth = isMobile() ? 230 : 384;
       const gap = isMobile() ? 4 : 8;
       const scrollPosition = (cardWidth + gap) * (index + 1);
       carouselRef.current.scrollTo({
@@ -72,9 +73,7 @@ export const Carousel = ({ items, initialScroll = 0 }) => {
   };
 
   return (
-    <CarouselContext.Provider
-      value={{ onCardClose: handleCardClose, currentIndex }}
-    >
+    <CarouselContext.Provider value={{ currentIndex }}>
       <div className="relative w-full">
         <div
           className="flex w-full overflow-x-scroll overscroll-x-auto py-10 md:py-20 scroll-smooth [scrollbar-width:none]"
@@ -83,15 +82,13 @@ export const Carousel = ({ items, initialScroll = 0 }) => {
         >
           <div
             className={cn(
-              "absolute right-0  z-[1000] h-auto  w-[5%] overflow-hidden bg-gradient-to-l"
+              "absolute right-0 z-[1000] h-auto  w-[5%] overflow-hidden bg-gradient-to-l"
             )}
           ></div>
 
           <div
             className={cn(
-              "flex flex-row justify-start gap-4 pl-4",
-              // remove max-w-4xl if you want the carousel to span the full width of its container
-              "max-w-7xl mx-auto"
+              "flex flex-row justify-start gap-4 pl-4 max-w-7xl mx-auto"
             )}
           >
             {items.map((item, index) => (
@@ -172,6 +169,8 @@ export const Card = ({ card, index, layout = false }) => {
     onCardClose(index);
   };
 
+  const router = useRouter();
+
   return (
     <>
       <AnimatePresence>
@@ -181,7 +180,7 @@ export const Card = ({ card, index, layout = false }) => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className="bg-black/80 backdrop-blur-lg h-full w-full fixed inset-0"
+              className="bg-black/80 backdrop-blur-lg h-full w-full fixed rounded-3xl inset-0"
             />
             <motion.div
               initial={{ opacity: 0 }}
@@ -205,11 +204,18 @@ export const Card = ({ card, index, layout = false }) => {
               </motion.p>
               <motion.p
                 layoutId={layout ? `title-${card.title}` : undefined}
-                className="text-2xl md:text-5xl font-semibold text-neutral-700 mt-4 dark:text-white"
+                className="text-lg md:text-2xl font-semibold text-neutral-700 mt-4 dark:text-white"
               >
                 {card.title}
               </motion.p>
-              <div className="py-10">{card.content}</div>
+              <div className="w-full flex justify-center mx-auto items-center mt-6">
+                <button
+                  className="px-2 py-1 dark:text-black text-white bg-black dark:bg-[#eee] rounded-lg"
+                  onClick={() => router.push(card.redirectRoute)}
+                >
+                  Shop Now
+                </button>
+              </div>
             </motion.div>
           </div>
         )}
